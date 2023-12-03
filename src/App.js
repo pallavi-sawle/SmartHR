11aa172dd68f349af5f54ab37735860465e560a0
pallavi-sawle <105306649+pallavi-sawle@users.noreply.github.com>
2023-12-03 19:28:12 +0530

    Add files via upload
    Adding src folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './SMARTHR-2/NavBar';
+import Subscription from './SMARTHR-2/Subscription';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Subscription />
+    </>
+  );
+}
+
+export default App;
